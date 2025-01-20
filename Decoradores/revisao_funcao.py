18c510d07eadabd06690ef104dac626b4cb0188c
Wise Muronha <wisenielsmuronha@gmail.com>
2025-01-20 22:02:20 +0200

    revisao de funcoes

---
+++ Decoradores/revisao_funcao.py
@@ -0,0 +1,67 @@
+# a = 20
+
+# a = 10
+
+
+# def soma(a, b=4):
+#     """esta é uma função de exemplo"""
+#     print(a + b)
+#     return a + b
+
+
+# print(soma.__qualname__)
+# print(soma.__doc__)
+# print(soma.__defaults__)
+# print(soma.__code__)
+
+
+# def faz_operacao(a, b, operacao):
+#     return operacao(a, b)
+
+
+# faz_operacao(10, 20, soma)
+
+# resulta = soma(10, 20)
+
+# lista = [soma]
+
+
+# def soma(a, b):
+#     res = a+b
+#     return res
+
+
+# soma(10, 20)
+# soma(10, 20)
+# soma(10, 20)
+# soma(10, 20)
+# a = 'Wise'
+
+a = 50
+
+
+# def funcao():
+
+#     a = 12
+
+#     def funcao2():
+#         print("oi")
+#         print(a)
+
+#     # print(True)
+#     funcao2()
+
+
+# # print(a)
+# funcao()
+
+
+a = 10
+b= 'Wise'
+
+print(type(a), hex(id(a)))
+print(type(b), hex(id(b)))
+print(type(None), hex(id(None)))
+
+
+
